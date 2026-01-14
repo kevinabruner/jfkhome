b077f4d075cd73523e14d2c73a709e63f24bc694
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-01-14 14:49:11 -0500

    trying to do it here

--- html/index.php
+++
@@ -1,7 +0,0 @@
-<h1>Welcome to thejfk.ca</h1>
-<p>This is a homelab domain for Kevin Bruner-Prime. Some of the hosted sites here include:</p>
-<ul>
-    <li><a href="https://nerdperk.ca">Nerdperk.ca</a></li>
-    <li><a href="https://ssvp.thejfk.ca">CRM for SSVP</a></li>
-    <li><a href="https://recursioncomic.com">Recursioncomic.com (webcomic)</a></li>
-</ul>
