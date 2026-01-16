73a127cc39a80e42db92df7c5a13e1cce02ec7f2
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-01-15 20:35:56 -0500

    two legends??

---
+++ html/legend.php
@@ -0,0 +1,10 @@
+<div class="legend-box">
+            <p><strong>Legend</strong> 
+            <ul class="status-list">
+                <li><span class="status-dot status-up" title="Status: up"></span>Site is up</li>
+                <li><span class="status-dot status-down" title="Status: down"></span>Site is down</li>
+                <li><span class="status-dot status-maint" title="Status: maint"></span>Site is under maintenance</li>
+                <li><span class="status-dot status-unknown" title="Status: unknown"></span>Site status is unknown</li>
+            </ul>
+            </p>
+        </div>
